0bacfe9cef5bd7e670c7933a46772aafc03749c8
Flávio André <flavio.andre@amcom.com.br>
2016-10-13 11:27:24 -0300

    inclusão

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,22 @@ gulp.task("thor", function(){
 	.pipe(gulp.dest("./dist/css"));
 } );
 
+
+gulp.task('buildjs', function(){
+	return gulp.src(['./source/components/jquery/dist/jquery.js',
+		'./source/components/jquery-mobile/jquery.mobile.custom.js'
+		])
+	.pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('aquaman',function(){
+	return gulp.src('./source/js/*.js')
+	.pipe(gulp.dest('./dist/js'));
+	});
+
 gulp.task('demolidor', function(){
-	gulp.watch('./source/sass/**/*.scss',['thor'])
-})
+	gulp.watch('./source/sass/**/*.scss',['thor']);
+	gulp.watch('./source/js/**/*.js',['aquaman']);
+});
+
+gulp.task('default',['buildjs','thor','aquaman','demolidor']);
